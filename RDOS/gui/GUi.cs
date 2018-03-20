3e9962e8eda09661a43c347aa6f048b1dfd83092
RobDevpwgt <34418637+RobDevpwgt@users.noreply.github.com>
2018-03-20 15:17:14 +0100

    update 1

---
+++ RDOS/gui/GUi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDOS
+{
+    class GUi
+    {
+        public static void mouse()
+        {
+            var m = new Cosmos.HAL.Mouse();
+        }
+    }
+}
---
+++ RDOS/gui/mouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDOS.gui
+{
+    class mouse
+    {
+        public static bool left()
+        {
+            var m = new Cosmos.HAL.Mouse();
+            if (Cosmos.HAL.Mouse.MouseState.Left == true)
+            {
+
+            }
+        }
+    }
+}
